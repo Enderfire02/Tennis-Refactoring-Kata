cadf958b7899b4b1b01467d8e0ece1bb0499e33a
Enzo Tremblay <contact@enzo-tremblay.fr>
2024-04-24 16:18:23 +0200

    TennisGame2 : Correction of all comments

---
+++ typescript/src/Player.ts
@@ -0,0 +1,11 @@
+// Class representing the player
+class Player {
+    // Name and points of the player
+    name: string;
+    points: number = 0;
+  
+    // Constructor that initializes the player with his name
+    constructor(name: string) {
+      this.name = name;
+    }
+  }
--- typescript/src/TennisGame2.ts
+++ typescript/src/TennisGame2.ts
@@ -1,121 +1,98 @@
 import { TennisGame } from './TennisGame';
 
+// Class representing the tennis game
 export class TennisGame2 implements TennisGame {
-  P1point: number = 0;
-  P2point: number = 0;
+  // Player 1 and player 2
+  private player1: Player;
+  private player2: Player;
 
-  P1res: string = '';
-  P2res: string = '';
+  // Point names
+  private readonly love: string = 'Love';
+  private readonly fifteen: string = 'Fifteen';
+  private readonly thirty: string = 'Thirty';
+  private readonly forty: string = 'Forty';
 
-  private player1Name: string;
-  private player2Name: string;
-
+  // Constructor initializing players with their respective names
   constructor(player1Name: string, player2Name: string) {
-    this.player1Name = player1Name;
-    this.player2Name = player2Name;
+    this.player1 = new Player(player1Name);
+    this.player2 = new Player(player2Name);
   }
 
+  // Method to get the current score of the game
   getScore(): string {
-    let score: string = '';
-    if (this.P1point === this.P2point && this.P1point < 4) {
-      if (this.P1point === 0)
-        score = 'Love';
-      if (this.P1point === 1)
-        score = 'Fifteen';
-      if (this.P1point === 2)
-        score = 'Thirty';
-      score += '-All';
-    }
-    if (this.P1point === this.P2point && this.P1point >= 3)
-      score = 'Deuce';
-
-    if (this.P1point > 0 && this.P2point === 0) {
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      if (this.P1point === 3)
-        this.P1res = 'Forty';
-
-      this.P2res = 'Love';
-      score = this.P1res + '-' + this.P2res;
-    }
-    if (this.P2point > 0 && this.P1point === 0) {
-      if (this.P2point === 1)
-        this.P2res = 'Fifteen';
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-
-      this.P1res = 'Love';
-      score = this.P1res + '-' + this.P2res;
-    }
-
-    if (this.P1point > this.P2point && this.P1point < 4) {
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      if (this.P1point === 3)
-        this.P1res = 'Forty';
-      if (this.P2point === 1)
-        this.P2res = 'Fifteen';
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
-    }
-    if (this.P2point > this.P1point && this.P2point < 4) {
-      if (this.P2point === 2)
-        this.P2res = 'Thirty';
-      if (this.P2point === 3)
-        this.P2res = 'Forty';
-      if (this.P1point === 1)
-        this.P1res = 'Fifteen';
-      if (this.P1point === 2)
-        this.P1res = 'Thirty';
-      score = this.P1res + '-' + this.P2res;
-    }
-
-    if (this.P1point > this.P2point && this.P2point >= 3) {
-      score = 'Advantage player1';
-    }
-
-    if (this.P2point > this.P1point && this.P1point >= 3) {
-      score = 'Advantage player2';
-    }
-
-    if (this.P1point >= 4 && this.P2point >= 0 && (this.P1point - this.P2point) >= 2) {
-      score = 'Win for player1';
-    }
-    if (this.P2point >= 4 && this.P1point >= 0 && (this.P2point - this.P1point) >= 2) {
-      score = 'Win for player2';
-    }
-    return score;
-  }
-
-  SetP1Score(score: number): void {
-    for (let i = 0; i < score; i++) {
-      this.P1Score();
+    if (this.isEqualScore()) {
+      return this.getEqualScore();
+    } else if (this.isAdvantageOrWinScore()) {
+      return this.getAdvantageOrWinScore();
+    } else {
+      return `${this.getScoreDescription(this.player1.points)}-${this.getScoreDescription(this.player2.points)}`;
     }
   }
 
-  SetP2Score(score: number): void {
-    for (let i = 0; i < score; i++) {
-      this.P2Score();
+  // Checks if both players' scores are equal and less than 4
+  private isEqualScore(): boolean {
+    return this.player1.points === this.player2.points && this.player1.points < 4;
+  }
+
+  // Checks if one of the players has a score of 4 or more
+  private isAdvantageOrWinScore(): boolean {
+    return this.player1.points >= 4 || this.player2.points >= 4;
+  }
+
+  // Returns the score in case of equality
+  private getEqualScore(): string {
+    if (this.player1.points < 3) {
+      return `${this.getScoreDescription(this.player1.points)}-All`;
+    } else {
+      return 'Deuce';
     }
   }
 
-  P1Score(): void {
-    this.P1point++;
+  // Returns the score in case of advantage or win
+  private getAdvantageOrWinScore(): string {
+    const scoreDifference: number = Math.abs(this.player1.points - this.player2.points);
+    if (scoreDifference === 1) {
+      return `Advantage ${this.getLeadingPlayer().name}`;
+    } else {
+      return `Win for ${this.getLeadingPlayer().name}`;
+    }
   }
 
-  P2Score(): void {
-    this.P2point++;
+  // Returns the description of a given score
+  private getScoreDescription(score: number): string {
+    switch (score) {
+      case 0: return this.love;
+      case 1: return this.fifteen;
+      case 2: return this.thirty;
+      case 3: return this.forty;
+      default: return '';
+    }
   }
 
-  wonPoint(player: string): void {
-    if (player === 'player1')
-      this.P1Score();
-    else
-      this.P2Score();
+  // Method overload to assign a point to a player
+  wonPoint(player: Player): void;
+  wonPoint(playerName: string): void;
+  wonPoint(arg: Player | string): void {
+    if (typeof arg === 'string') {
+      this.wonPointByName(arg);
+    } else {
+      arg.points++;
+    }
+  }
+
+  // Assigns a point to a player based on their name
+  private wonPointByName(playerName: string): void {
+    if (playerName === this.player1.name) {
+      this.player1.points++;
+    } else if (playerName === this.player2.name) {
+      this.player2.points++;
+    } else {
+      throw new Error('Invalid player name');
+    }
+  }
+
+  // Returns the player with the highest score
+  private getLeadingPlayer(): Player {
+    return this.player1.points > this.player2.points ? this.player1 : this.player2;
   }
 }
